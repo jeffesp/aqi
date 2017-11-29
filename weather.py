b632894f4a2b3951ea3b4c972c49b1db5681fa4a
Jeff Espenschied <jeffesp@nexosis.com>
2017-11-29 06:48:20 -0500

    weather module started

---
+++ weather.py
@@ -0,0 +1,32 @@
+from os import environ
+from datetime import date, datetime, timedelta
+from darksky import forecast
+
+COLS = 40.0859627, -83.0111861
+BEIJING = 39.954352, 116.466258
+
+
+def get_weather(lat, lng, start_date=None, num_days=1):
+    def make_request(dte):
+        with forecast(key, lat, lng, time=int(dte.timestamp())) as fcst:
+            return fcst.daily[0]
+
+    key = environ.get('DARK_SKY_KEY')
+    if start_date is None:
+        start_date = datetime.today()
+    dates = [start_date + timedelta(days=d) for d in range(0, num_days)]
+
+    return [make_request(d) for d in dates]
+
+
+def print_weather(day):
+    data = dict(day=date.strftime(date.fromtimestamp(day.time), '%a'),
+                sum=day.summary,
+                tMin=day.temperatureMin,
+                tMax=day.temperatureMax)
+    print('{day}: {sum} Temp range: {tMin} - {tMax}'.format(**data))
+
+
+if __name__ == '__main__':
+    [print_weather(d) for d in get_weather(*COLS, num_days=10)]
+    
