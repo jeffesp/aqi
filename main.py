7d9afcffee8dc363e3c94780685f3ff7eb95a832
Jeff Espenschied <jeffesp@nexosis.com>
2017-11-30 13:32:39 -0500

    Update weather to just use `requests` lib as it's easier.

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import json
 import sqlite3
 
-from weather import get_weather, COLS
-from data import ensure_db_exists, add_weather 
+from weather import get_weather
+from data import ensure_db_exists, add_weather
 
+COLS = 40.0859627, -83.0111861
+BEIJING = 39.954352, 116.466258
 
 db = sqlite3.connect('aqi.sqlite')
 ensure_db_exists(db)
--- weather.py
+++ weather.py
@@ -1,32 +1,50 @@
 from os import environ
 from datetime import date, datetime, timedelta
-from darksky import forecast
+import requests
+import backoff
 
-COLS = 40.0859627, -83.0111861
-BEIJING = 39.954352, 116.466258
-
+BASE_URL = 'https://api.darksky.net/forecast/{}/{},{},{}?exclude=currently,minutely,flags'
 
 def get_weather(lat, lng, start_date=None, num_days=1):
-    def make_request(dte):
-        with forecast(key, lat, lng, time=int(dte.timestamp())) as fcst:
-            return fcst.daily[0]
+
+    def fatal_code(e):
+        return 400 <= e.response.status_code < 500
+
+    @backoff.on_exception(
+        backoff.expo,
+        (requests.exceptions.Timeout, requests.exceptions.ConnectionError),
+        max_tries=4,
+        giveup=fatal_code)
+    def make_request(key, dte):
+        accept_gzip = {'accept-encoding': 'gzip'}
+        since_epoch = int(dte.timestamp())
+        response = requests.get(BASE_URL.format(key, lat, lng, since_epoch), headers=accept_gzip)
+        response.raise_for_status()
+        return response.json()
+
+    if num_days < 1:
+        raise Exception('Cannot get forecast for fewer than one day.')
 
     key = environ.get('DARK_SKY_KEY')
     if start_date is None:
         start_date = datetime.today()
     dates = [start_date + timedelta(days=d) for d in range(0, num_days)]
 
-    return [make_request(d) for d in dates]
+    if num_days == 1:
+        return make_request(key, dates.pop())
+    else:
+        return [make_request(key, d) for d in dates]
 
 
-def print_weather(day):
-    data = dict(day=date.strftime(date.fromtimestamp(day.time), '%a'),
-                sum=day.summary,
-                tMin=day.temperatureMin,
-                tMax=day.temperatureMax)
+def print_weather(response):
+    day = response['daily']['data'][0]
+    data = dict(day=date.strftime(date.fromtimestamp(day['time']), '%a'),
+                sum=day['summary'],
+                tMin=day['temperatureMin'],
+                tMax=day['temperatureMax'])
     print('{day}: {sum} Temp range: {tMin} - {tMax}'.format(**data))
 
 
 if __name__ == '__main__':
-    _ = [print_weather(d) for d in get_weather(*COLS, num_days=10)]
+    _ = [print_weather(d) for d in get_weather(40.0859627, -83.0111861, num_days=3)]
     
