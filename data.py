94787eaab9756f94af8c9a5c07311984c0ed0581
Jeff Espenschied <jeffesp@nexosis.com>
2017-12-07 06:04:13 -0500

    Getting and adding weather without a lib.

--- data.py
+++ data.py
@@ -6,10 +6,12 @@ CREATE_WEATHER = '''CREATE TABLE IF NOT EXISTS weather (
             ts timestamp NOT NULL,
             value TEXT);'''
 CREATE_WEATHER_INDEX = '\
-    CREATE INDEX IF NOT EXISTS query_values ON weather (lat, long, ts);'
+    CREATE INDEX IF NOT EXISTS query_values ON weather (lat, long, ts DESC);'
 INSERT_WEATHER = 'INSERT INTO weather VALUES (?, ?, ?, ?);'
 SELECT_WEATHER = '\
     SELECT value FROM weather WHERE lat = ? AND long = ? AND ts = ?;'
+SELECT_LATEST_WEATHER = '\
+    SELECT value FROM weather WHERE lat = ? AND long = ? ORDER BY ts DESC LIMIT 1;'
 
 def ensure_db_exists(data):
     data.execute(CREATE_WEATHER)
@@ -22,6 +24,10 @@ def add_weather(data, *args):
 def find_weather(data, lat, lng, ts):
     return data.execute(SELECT_WEATHER, (lat, lng, ts))
 
+def get_latest_weather(data, lat, lng):
+    return data.execute(SELECT_LATEST_WEATHER, (lat, lng))
+
 if __name__ == '__main__':
     db = sqlite3.connect('aqi.sqlite')
     ensure_db_exists(db)
+    get_latest_weather(db, 39.954352, 116.466258)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import json
 import sqlite3
 
-from weather import get_weather
+from weather import get_weather, print_weather
 from data import ensure_db_exists, add_weather
 
 COLS = 40.0859627, -83.0111861
@@ -11,8 +11,9 @@ BEIJING = 39.954352, 116.466258
 db = sqlite3.connect('aqi.sqlite')
 ensure_db_exists(db)
 
-w = get_weather(*COLS)
+w = get_weather(*BEIJING)
 
-add_weather(db, COLS[0], COLS[1], datetime.today(), json.dumps(w[0]._data))
+print_weather(w)
+add_weather(db, BEIJING[0], BEIJING[1], datetime.today(), json.dumps(w))
 
 db.commit()
